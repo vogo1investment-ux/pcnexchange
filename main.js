109e1d9f3b1e770ecac90f283ab11ec45afb800d
vogo1investment-ux <vogo1investment@gmail.com>
2026-05-28 14:56:10 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
 import {
+
 initializeApp
+
 }
+
 from
+
 "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 
 import {
+
 getAuth,
-signInWithEmailAndPassword,
+
 createUserWithEmailAndPassword,
+
+signInWithEmailAndPassword,
+
 onAuthStateChanged
+
 }
+
 from
+
 "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 
 import {
+
 getFirestore,
+
 doc,
-setDoc
+
+setDoc,
+
+getDoc
+
 }
+
 from
+
 "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
 
 const firebaseConfig={
@@ -33,98 +52,23 @@ storageBucket:"jumiastaff-83757.firebasestorage.app",
 
 messagingSenderId:"1018307795636",
 
-appId:"1:1018307795636:web:6545b94e234fe9fb1ad5e1",
-
-measurementId:"G-W9M358ZS1G"
+appId:"1:1018307795636:web:6545b94e234fe9fb1ad5e1"
 
 };
 
 const app=
+
 initializeApp(firebaseConfig);
 
 const auth=
+
 getAuth(app);
 
 const db=
+
 getFirestore(app);
 
-const coins=[
-
-{
-name:"BTC",
-price:"$72,981.69",
-change:"-2.89%"
-},
-
-{
-name:"ETH",
-price:"$1,983.19",
-change:"-3.99%"
-},
-
-{
-name:"BNB",
-price:"$631.41",
-change:"-3.22%"
-},
-
-{
-name:"XRP",
-price:"$1.29",
-change:"-2.45%"
-},
-
-{
-name:"USDT",
-price:"$1.00",
-change:"+0.01%"
-}
-
-];
-
-coins.forEach(c=>{
-
-coins.innerHTML+=`
-
-<div class="coin">
-
-<div>
-
-<h2>
-
-${c.name}
-
-</h2>
-
-</div>
-
-<div>
-
-<h2>
-
-${c.price}
-
-</h2>
-
-</div>
-
-<div class="${
-c.change.includes('-')
-?'red'
-:'green'
-}">
-
-${c.change}
-
-</div>
-
-</div>
-
-`;
-
-});
-
-let mode="login";
+let signupMode=false;
 
 window.openLogin=()=>{
 
@@ -134,7 +78,7 @@ authTitle.innerHTML="LOGIN";
 
 authBtn.innerHTML="LOGIN";
 
-mode="login";
+signupMode=false;
 
 };
 
@@ -146,7 +90,7 @@ authTitle.innerHTML="SIGN UP";
 
 authBtn.innerHTML="CREATE ACCOUNT";
 
-mode="signup";
+signupMode=true;
 
 };
 
@@ -156,60 +100,69 @@ authBox.style.display="none";
 
 };
 
-authBtn.onclick=async()=>{
+authBtn.onclick=
 
-if(mode=="login"){
+async()=>{
 
-try{
+let userEmail=email.value;
 
-await signInWithEmailAndPassword(
+let userPassword=password.value;
 
-auth,
+if(
 
-email.value,
+!userEmail ||
 
-password.value
+!userPassword
+
+){
+
+alert(
+
+"Fill all fields"
 
 );
 
-location="dashboard.html";
+return;
 
 }
-
-catch(e){
-
-alert("LOGIN FAILED");
-
-}
-
-}else{
 
 try{
 
-let userCredential=
+if(signupMode){
+
+const userCredential=
 
 await createUserWithEmailAndPassword(
 
 auth,
 
-email.value,
+userEmail,
 
-password.value
+userPassword
 
 );
+
+const user=
+
+userCredential.user;
 
 await setDoc(
 
 doc(
+
 db,
+
 "users",
-userCredential.user.uid
+
+user.uid
+
 ),
 
 {
 
-email:
-email.value,
+username:
+
+userEmail.split("@")[0],
 
 availableBalance:0,
 
@@ -219,9 +172,17 @@ referralCommission:0,
 
 joinedUsers:0,
 
+usedReferral:"",
+
 cart:[]
 
 }
+
+);
+
+alert(
+
+"ACCOUNT CREATED"
 
 );
 
@@ -229,11 +190,39 @@ location="dashboard.html";
 
 }
 
-catch(e){
+else{
 
-alert("SIGNUP FAILED");
+await signInWithEmailAndPassword(
+
+auth,
+
+userEmail,
+
+userPassword
+
+);
+
+alert(
+
+"LOGIN SUCCESS"
+
+);
+
+location="dashboard.html";
 
 }
+
+}
+
+catch(e){
+
+alert(
+
+e.message
+
+);
+
+console.log(e);
 
 }
 
@@ -245,10 +234,22 @@ auth,
 
 user=>{
 
-if(user){
+if(
+
+user &&
+
+location.pathname.includes(
+
+"index.html"
+
+)
+
+){
 
 console.log(
-"LOGGED IN"
+
+"USER LOGGED IN"
+
 );
 
 }
