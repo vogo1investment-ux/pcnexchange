23d97d8f036d415b6b0a91b5d6930a6d6d6623de
vogo1investment-ux <vogo1investment@gmail.com>
2026-06-07 23:21:28 +0100

    Add files via upload

---
+++ admin/notification.js
@@ -0,0 +1,100 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+
+import {
+getFirestore,
+collection,
+addDoc,
+serverTimestamp
+}
+from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyCQVHBn504Y26YtR38JRJhRlUbBoa2CIPo",
+
+authDomain: "pcnexchange.firebaseapp.com",
+
+databaseURL: "https://pcnexchange-default-rtdb.firebaseio.com",
+
+projectId: "pcnexchange",
+
+storageBucket: "pcnexchange.firebasestorage.app",
+
+messagingSenderId: "278761036604",
+
+appId: "1:278761036604:web:a02e2d2ac7a9379d6f9c39"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+document
+.getElementById("sendBtn")
+.addEventListener("click", async()=>{
+
+const userId =
+document.getElementById("userId").value.trim();
+
+const title =
+document.getElementById("title").value.trim();
+
+const message =
+document.getElementById("message").value.trim();
+
+const status =
+document.getElementById("status");
+
+if(!title || !message){
+
+status.style.color="red";
+status.innerText="Please enter title and message.";
+
+return;
+
+}
+
+status.style.color="white";
+status.innerText="Sending...";
+
+try{
+
+await addDoc(
+collection(db,"notifications"),
+{
+title:title,
+message:message,
+
+target:
+userId
+? "user"
+: "all",
+
+userId:
+userId || null,
+
+createdAt:
+serverTimestamp()
+}
+);
+
+status.style.color="#00ff88";
+status.innerText="Notification Sent Successfully";
+
+document.getElementById("userId").value="";
+document.getElementById("title").value="";
+document.getElementById("message").value="";
+
+}
+catch(error){
+
+console.error(error);
+
+status.style.color="red";
+status.innerText=error.message;
+
+}
+
+});
